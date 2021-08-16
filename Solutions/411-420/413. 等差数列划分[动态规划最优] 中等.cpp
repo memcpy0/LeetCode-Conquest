95a62237012f37ed0e5550093674e53a1da188c3
memcpy0 <2183927003@qq.com>
2021-08-16 22:29:11 +0800

    :sparkles:feat: Add a best dynamic programming solution of problem 413

---
+++ Solutions/411-420/413. 等差数列划分[动态规划最优] 中等.cpp
@@ -0,0 +1,18 @@
+/*
+** Problem: https://leetcode-cn.com/problems/arithmetic-slices
+** Article: https://memcpy0.blog.csdn.net/article/details/119641437
+*/
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        if (nums.size() <= 2) return 0;
+        int n = nums.size(), ans = 0, dp = 0;
+        for (int i = 2; i < n; ++i) {
+            if (nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) {
+                ++dp; //dp[i]=dp[i-1]+1;
+                ans += dp;
+            } else dp = 0;
+        } 
+        return ans;
+    }
+};
