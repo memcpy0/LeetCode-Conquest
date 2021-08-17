d1fe2e079fbbb5b45ee1756c06bfb0a805850dba
memcpy0 <2183927003@qq.com>
2021-08-17 23:50:16 +0800

    :sparkles:feat: Add best solutions for problem 233 and 551 respectively

---
+++ Solutions/551-560/551. 学生出勤记录 I[字符串] 简单.cpp
@@ -0,0 +1,15 @@
+/*
+** Problem: https://leetcode-cn.com/problems/student-attendance-record-i/
+** Article: https://memcpy0.blog.csdn.net/article/details/109556361
+*/
+class Solution {
+public:
+    bool checkRecord(string s) {
+        int a = 0; //absent
+        for (int i = 0, n = s.size(); i < n; ++i) {
+            if (s[i] == 'A' && ++a >= 2) return false;
+            else if (s[i] == 'L' && i < n - 2 && s[i + 1] == 'L' && s[i + 2] == 'L') return false;
+        }
+        return true;
+    }
+};
