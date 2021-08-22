b23d455027dba7e15955a0f26188539d1f226367
memcpy0 <2183927003@qq.com>
2021-08-22 20:32:16 +0800

    ✨feat: Add 1 best solution of problem 443

---
+++ Solutions/441-450/443. String Compression[双指针] 中等.cpp
@@ -0,0 +1,26 @@
+/*
+** Problem: https://leetcode-cn.com/problems/string-compression/
+** Article: https://memcpy0.blog.csdn.net/article/details/119845202
+** Author: memcpy0
+*/
+class Solution {
+public:
+    int compress(vector<char>& chars) { 
+        int ans = 0;
+        for (int i = 0, len = 1, n = chars.size(); i < n; ++i, ++len) {
+            if (i + 1 != n && chars[i] == chars[i + 1]) continue;
+            chars[ans++] = chars[i];
+            if (len > 1) {
+                int l = ans;
+                while (len) {
+                    chars[ans++] = '0' + len % 10;
+                    len /= 10;
+                }
+                int r = ans - 1;
+                while (l < r) swap(chars[l++], chars[r--]);
+            }
+            len = 0;
+        } 
+        return ans;
+    }
+};
