cfd1ad7d2fbdae107f55054becde0b996d1589f9
memcpy0 <2183927003@qq.com>
2021-09-29 15:22:06 +0800

    ✨feat: Add 1 best solution of problem 58

---
+++ Solutions/51-60/58. 最后一个单词的长度[模拟] 简单.cpp
@@ -0,0 +1,14 @@
+/*
+** Problem: https://leetcode-cn.com/problems/length-of-last-word/
+** Article: 
+** Author: memcpy0
+*/
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int j = s.size() - 1, len = 0;
+        while (j >= 0 && s[j] == ' ') --j; //跳过空格
+        while (j >= 0 && s[j] != ' ') ++len, --j; //计算最后一个单词长度
+        return len;
+    }
+};
