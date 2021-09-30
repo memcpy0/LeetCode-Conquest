f982b1476497149fd8b94bf2b95675c76c9a0436
memcpy0 <2183927003@qq.com>
2021-10-01 03:31:55 +0800

    ✨feat: Add 1 best solution of problem 2018

---
+++ Solutions/2011-2020/2018. 判断单词是否能放入填字游戏内[DFS,剪枝,数组] 中等.cpp
@@ -0,0 +1,42 @@
+/*
+** Problem: https://leetcode-cn.com/problems/check-if-word-can-be-placed-in-crossword/
+** Article: 
+** Author: memcpy0
+*/
+class Solution {
+private:
+    int d[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    bool dfs(const vector<vector<char>>& b, const string& w, int i, int j, int k, int cur) {
+        if (cur >= w.size()) return true;
+        if (b[i][j] == '#' || (b[i][j] != ' ' && b[i][j] != w[cur])) return false;
+        return dfs(b, w, i + d[k][0], j + d[k][1], k, cur + 1);
+    }
+public:
+    bool placeWordInCrossword(vector<vector<char>>& board, string word) {
+        int m = board.size(), n = board[0].size(), len = word.size();
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (board[i][j] != '#' && (board[i][j] == ' ' || board[i][j] == word[0])) {
+                    int ti0 = i + len - 1, ti1 = i - len + 1, tj0 = j + len - 1, tj1 = j - len + 1; 
+                    if ((ti0 < m && board[ti0][j] != '#') && //up to down
+                        (i == 0 || board[i - 1][j] == '#') &&
+                        (ti0 == m - 1 || board[ti0 + 1][j] == '#') &&
+                        dfs(board, word, i, j, 0, 0)) return true;
+                    if ((ti1 >= 0 && board[ti1][j] != '#') && //down to up
+                        (i == m - 1 || board[i + 1][j] == '#') &&
+                        (ti1 == 0 || board[ti1 - 1][j] == '#') &&
+                        dfs(board, word, i, j, 1, 0)) return true;
+                    if ((tj0 < n && board[i][tj0] != '#') && //left to right
+                        (j == 0 || board[i][j - 1] == '#') &&
+                        (tj0 == n - 1 || board[i][tj0 + 1] == '#') &&
+                        dfs(board, word, i, j, 2, 0)) return true;
+                    if ((tj1 >= 0 && board[i][tj1] != '#') && //right to left
+                        (j == n - 1 || board[i][j + 1] == '#') &&
+                        (tj1 == 0 || board[i][tj1 - 1] == '#') &&
+                        dfs(board, word, i, j, 3, 0)) return true;
+                }
+            }
+        }
+        return false;
+    }
+};
