24c42f6e44946ef285da32d91497a5db0377a48f
memcpy0 <2183927003@qq.com>
2021-12-23 21:30:45 +0800

    ✨feat: Add 1 best solution of problem 319

---
+++ Solutions/311-320/319. 灯泡开关[数论] 中等.java
@@ -0,0 +1,10 @@
+/*
+** Problem: https://leetcode-cn.com/problems/bulb-switcher/
+** Article: https://memcpy0.blog.csdn.net/article/details/122116523
+** Author: memcpy0
+*/
+class Solution {
+    public int bulbSwitch(int n) {
+        return (int)Math.sqrt(n);
+    }
+}
