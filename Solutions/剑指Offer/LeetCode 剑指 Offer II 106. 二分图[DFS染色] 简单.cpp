9c1dbd7de972c3b13a1756f8a794a4c4fc2d7c84
memcpy0 <2183927003@qq.com>
2023-04-17 14:31:33 +0800

    ✨feat: Add 1 best solution of problem 剑指Offer II 106

---
+++ Solutions/剑指Offer/LeetCode 剑指 Offer II 106. 二分图[DFS染色] 简单.cpp
@@ -0,0 +1,23 @@
+/*
+** Problem: https://leetcode.cn/problems/vEAB3K/
+** Article: https://memcpy0.blog.csdn.net/article/details/130199118
+** Author: memcpy0
+*/
+class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> color(n);
+        function<bool(int, int)> dfs = [&](int i, int c) {
+            color[i] = c; // 1,2表示访问过,0表示未访问
+            for (int j : graph[i]) {
+                if (!color[j] && dfs(j, 3 - c) == false) return false;
+                else if (color[j] == color[i]) {  return false; }
+            }
+            return true;
+        };
+        for (int i = 0; i < n; ++i)
+            if (!color[i] && dfs(i, 1) == false) return false;
+        return true;
+    }
+};
