dd84e1f12eba892b916f17f49a05948cd1e29480
memcpy0 <2183927003@qq.com>
2023-04-22 13:07:38 +0800

    ✏️typos: Change source code in solution of problem 1218

--- Solutions/1211-1220/1218. 最长定差子序列[动态规划,哈希表(整型数组),最优].cpp
+++ Solutions/1211-1220/1218. 最长定差子序列[动态规划,哈希表(整型数组),最优].cpp
@@ -6,6 +6,17 @@
 class Solution {
 public:
     int longestSubsequence(vector<int>& arr, int difference) {
+        unordered_map<int, int> dp;
+        int ans = 0;
+        for (const int &i : arr)
+            ans = max(ans, dp[i] = dp[i - difference] + 1);
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    int longestSubsequence(vector<int>& arr, int difference) {
         const int N = 40010, M = N / 2;
         vector<int> dp(N);
         int ans = 0;
