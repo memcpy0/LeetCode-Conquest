3a00bb7ead611ac42c5e5a3a4920215be8e398d9
memcpy0 <2183927003@qq.com>
2023-05-20 11:24:04 +0800

    ✨feat: Add 1 best solution of problem 1373

---
+++ Solutions/1371-1380/1373. Maximum Sum BST in Binary Tree.cpp
@@ -0,0 +1,26 @@
+/*
+** Problem: https://leetcode.cn/problems/maximum-sum-bst-in-binary-tree/
+** Article: https://memcpy0.blog.csdn.net/article/details/
+** Author: memcpy0
+*/
+class Solution {
+public:
+    int maxsum = 0;
+    int maxSumBST(TreeNode* root) {
+        dfs(root);
+        return maxsum;
+    }
+    vector<int> dfs(TreeNode* root) {
+        if (!root) return {true, INT_MAX, INT_MIN, 0};
+        auto lArr = dfs(root->left);
+        auto rArr = dfs(root->right);
+        int sum = 0, curmax, curmin;
+        if (!lArr[0] || !rArr[0] || root->val >= rArr[1] || root->val <= lArr[2]) 
+            return {false, 0, 0, 0};
+        curmin = root->left ? lArr[1] : root->val;
+        curmax = root->right ? rArr[2] : root->val;
+        sum += (root->val + lArr[3] + rArr[3]);
+        maxsum = max(maxsum, sum);
+        return {true, curmin, curmax, sum};
+    }
+};
