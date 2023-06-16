abe3b8d2d20e6cc4c3ed3905aef8d1c92c70c8bf
memcpy0 <2183927003@qq.com>
2023-06-16 14:52:34 +0800

    ✨feat: Add 1 best solution of problem 2475

---
+++ Solutions/2471-2480/2475. Number of Unequal Triplets in Array[暴力;排序;哈希表] 简单.cpp
@@ -0,0 +1,49 @@
+/*
+** Problem: https://leetcode.cn/problems/number-of-unequal-triplets-in-array
+** Article: https://memcpy0.blog.csdn.net/article/details/
+** Author: memcpy0
+*/
+// 哈希表
+class Solution {
+    public int unequalTriplets(int[] nums) {
+        int[] count = new int[1001];
+        for (int i : nums) ++count[i];
+        int a = 0, b = 0, c = nums.length;
+        int ans = 0;
+        for (int i = 1; i <= 1000; ++i) {
+            if (count[i] != 0) {
+                c -= count[i];
+                ans += a * count[i] * c;
+                a += count[i];
+            }
+        }
+        return ans;
+    }
+}
+
+// 排序+分组统计
+class Solution {
+    public int unequalTriplets(int[] nums) {
+        Arrays.sort(nums);
+        int ans = 0, start = 0;
+        for (int i = 0; i + 1 < nums.length; ++i) {
+            if (nums[i] != nums[i + 1]) { // 到一段的末尾
+                ans += start * (i - start + 1) * (nums.length - 1 - i);
+                start = i + 1;
+            }
+        } 
+        return ans;
+    }
+}
+
+// 暴力循环
+class Solution {
+    public int unequalTriplets(int[] nums) {
+        int ans = 0;
+        for (int i = 0; i < nums.length; ++i)
+            for (int j = i + 1; j < nums.length; ++j)
+                for (int k = j + 1; k < nums.length; ++k)
+                    if (nums[i] != nums[j] && nums[j] != nums[k] && nums[i] != nums[k]) ++ans;
+        return ans;
+    }
+}
