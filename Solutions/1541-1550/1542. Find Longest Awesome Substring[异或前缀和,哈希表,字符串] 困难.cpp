ec493bae3df54eba42e5332c8940b524ab3f70c2
memcpy0 <2183927003@qq.com>
2023-06-20 22:01:52 +0800

    ✨feat: Add 1 best solution of problem 1542

---
+++ Solutions/1541-1550/1542. Find Longest Awesome Substring[异或前缀和,哈希表,字符串] 困难.cpp
@@ -0,0 +1,22 @@
+/*
+** Problem: https://leetcode.cn/problems/find-longest-awesome-substring/
+** Article: https://memcpy0.blog.csdn.net/article/details/131316399
+** Author: memcpy0
+*/
+class Solution {
+public:
+    int longestAwesome(string s) {
+        int n = s.size(), xor_sum = 0, ans = 0;
+        int rec[1024];
+        memset(rec, -1, sizeof(rec)); // 空前缀对应键值对(0,-1)
+        for (int i = 0; i < n; ++i) {
+            xor_sum ^= 1 << (s[i] - '0'); // 特判异或前缀和为0的情况
+            if (xor_sum == 0 || ~rec[xor_sum]) ans = max(ans, i - rec[xor_sum]);
+            else rec[xor_sum] = i;
+            for (int j = 0; j < 10; ++j)
+                if (~rec[xor_sum ^ (1 << j)])
+                    ans = max(ans, i - rec[xor_sum ^ (1 << j)]);
+        }
+        return ans;
+    }
+};
