d453b4e20327f4581f817649c45bd38c7fca4d25
memcpy0 <2183927003@qq.com>
2023-09-09 13:37:55 +0800

    ✨feat: Add 1 best and simplest solution of problem 119, 118

---
+++ Solutions/111-120/118. Pascal's Triangle[数组,数学] 简单.cpp
@@ -0,0 +1,19 @@
+/*
+** Problem: https://leetcode.cn/problems/pascals-triangle/
+** Article: https://memcpy0.blog.csdn.net/article/details/109192803
+** Author: memcpy0
+*/
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ret(numRows);
+        for (int i = 0; i < numRows; ++i) {
+            ret[i].resize(i + 1);
+            ret[i][0] = ret[i][i] = 1;
+            for (int j = 1; j < i; ++j) {
+                ret[i][j] = ret[i - 1][j] + ret[i - 1][j - 1];
+            }
+        }
+        return ret;
+    }
+};
