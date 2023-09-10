a2ba570753724c32be22d32c0d453c0f5aaf1f03
memcpy0 <2183927003@qq.com>
2023-09-10 16:10:14 +0800

    ✨feat: Add best and simplest solutions for problem 1359

---
+++ Solutions/1351-1360/1359. Count All Valid Pickup and Delivery Options[组合数学,动态规划] 困难.cpp
@@ -0,0 +1,20 @@
+/*
+** Problem: https://leetcode.cn/problems/count-all-valid-pickup-and-delivery-options
+** Article: https://memcpy0.blog.csdn.net/article/details/
+** Author: memcpy0
+*/
+// BEST
+using LL = long long;
+
+class Solution {
+private:
+    static constexpr int mod = 1000000007;
+public:
+    int countOrders(int n) {
+        if (n == 1) return 1;
+        int ans = 1;
+        for (int i = 2; i <= n; ++i)
+            ans = (LL)ans * (i * 2 - 1) % mod * i % mod;
+        return ans;
+    }
+};
