35cad531092e2e558f0b93535c4b9fbc5d34d8f8
memcpy0 <2183927003@qq.com>
2023-09-13 13:51:07 +0800

    ✨feat: Add 1 best and simplest solution for problem 135

---
+++ Solutions/2591-2600/135. Candy[贪心,一次遍历] 困难.cpp
@@ -0,0 +1,55 @@
+/*
+** Problem: https://leetcode.cn/problems/candy
+** Article: https://memcpy0.blog.csdn.net/article/details/111602874
+** Author: memcpy0
+*/
+// BEST: 一次遍历+常量空间
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        int ret = 1;
+        int inc = 1, dec = 0, pre = 1;
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] >= ratings[i - 1]) {
+                dec = 0;
+                pre = ratings[i] == ratings[i - 1] ? 1 : pre + 1;
+                ret += pre;
+                inc = pre;
+            } else {
+                dec++;
+                if (dec == inc) {
+                    dec++;
+                }
+                ret += dec; // 给递减序列（包括当前同学）再分一个糖果
+                pre = 1; // 表示已经给当前同学分一个糖果（上一步中）
+            }
+        }
+        return ret;
+    }
+};
+// 拆分为两个规则+两次遍历
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> left(n);
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && ratings[i] > ratings[i - 1]) {
+                left[i] = left[i - 1] + 1;
+            } else {
+                left[i] = 1;
+            }
+        }
+        int right = 0, ret = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (i < n - 1 && ratings[i] > ratings[i + 1]) {
+                right++;
+            } else {
+                right = 1;
+            }
+            ret += max(left[i], right);
+        }
+        return ret;
+    }
+};
