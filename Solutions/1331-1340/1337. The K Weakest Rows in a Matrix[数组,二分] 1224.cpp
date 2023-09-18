7aac1eb6a9a8f2e713ab80b2e2b42815f6a43c24
memcpy0 <2183927003@qq.com>
2023-09-18 23:31:11 +0800

    ✨feat: Add 1 best but complex solution for problem 1337

---
+++ Solutions/1331-1340/1337. The K Weakest Rows in a Matrix[数组,二分] 1224.cpp
@@ -0,0 +1,111 @@
+/*
+** Problem: https://leetcode.cn/problems/the-k-weakest-rows-in-a-matrix/
+** Article: https://memcpy0.blog.csdn.net/article/details/133001767
+** Author: memcpy0
+*/
+// BEST: 最好写
+class Solution {
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        int m = mat.size(), n = mat[0].size();
+        vector<pair<int, int>> power;
+        for (int i = 0; i < m; ++i) {
+            int l = 0, r = n - 1, pos = -1;
+            while (l <= r) {
+                int mid = (l + r) / 2;
+                if (mat[i][mid] == 0) {
+                    r = mid - 1;
+                }
+                else {
+                    pos = mid;
+                    l = mid + 1;
+                }
+            }
+            power.emplace_back(pos + 1, i);
+        }
+        priority_queue q(greater<pair<int, int>>(), move(power));
+        vector<int> ans;
+        for (int i = 0; i < k; ++i) {
+            ans.push_back(q.top().second);
+            q.pop();
+        }
+        return ans;
+    }
+};
+// 快速选择+排序前k个元素
+template<typename T>
+class Helper {
+    static int partition(vector<T>& nums, int l, int r) {
+        T pivot = nums[r];
+        int i = l - 1;
+        for (int j = l; j <= r - 1; ++j) {
+            if (nums[j] <= pivot) {
+                i = i + 1;
+                swap(nums[i], nums[j]);
+            }
+        }
+        swap(nums[i + 1], nums[r]);
+        return i + 1;
+    }
+
+    // 基于随机的划分
+    static int randomized_partition(vector<T>& nums, int l, int r) {
+        int i = rand() % (r - l + 1) + l;
+        swap(nums[r], nums[i]);
+        return partition(nums, l, r);
+    }
+
+    static void randomized_selected(vector<T>& arr, int l, int r, int k) {
+        if (l >= r) {
+            return;
+        }
+        int pos = randomized_partition(arr, l, r);
+        int num = pos - l + 1;
+        if (k == num) {
+            return;
+        } else if (k < num) {
+            randomized_selected(arr, l, pos - 1, k);
+        } else {
+            randomized_selected(arr, pos + 1, r, k - num);
+        }
+    }
+
+public:
+    static vector<T> getLeastNumbers(vector<T>& arr, int k) {
+        srand((unsigned)time(NULL));
+        randomized_selected(arr, 0, (int)arr.size() - 1, k);
+        vector<T> vec;
+        for (int i = 0; i < k; ++i) {
+            vec.push_back(arr[i]);
+        }
+        return vec;
+    }
+};
+class Solution {
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        int m = mat.size(), n = mat[0].size();
+        vector<pair<int, int>> power;
+        for (int i = 0; i < m; ++i) {
+            int l = 0, r = n - 1, pos = -1;
+            while (l <= r) {
+                int mid = (l + r) / 2;
+                if (mat[i][mid] == 0) {
+                    r = mid - 1;
+                }
+                else {
+                    pos = mid;
+                    l = mid + 1;
+                }
+            }
+            power.emplace_back(pos + 1, i);
+        }
+        vector<pair<int, int>> minimum = Helper<pair<int, int>>::getLeastNumbers(power, k);
+        sort(minimum.begin(), minimum.begin() + k);
+        vector<int> ans;
+        for (int i = 0; i < k; ++i) {
+            ans.push_back(minimum[i].second);
+        }
+        return ans;
+    }
+};
